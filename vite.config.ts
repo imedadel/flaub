6e943e3c4c2347d9b7c9b09360c4f0e6551fa762
Imed Adel <aadelimed@gmail.com>
2021-10-22 08:51:28 +0100

    Remove unused

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,7 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
-import path from "path"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	// resolve: {
-	// 	alias: [
-	// 		{
-	// 			find: /^es5-ext\/(.*)?\/#\/(.*)$/,
-	// 			replacement: path.resolve(require.resolve("es5-ext"), "../$1/../#/$2"),
-	// 		},
-	// 		{
-	// 			find: /^.\/#$/,
-	// 			replacement: "../#/index.js",
-	// 		},
-	// 	],
-	// },
 	plugins: [react()],
 })
