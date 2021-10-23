102363259b509153519bb7ef4828c426fae636c1
Imed Adel <aadelimed@gmail.com>
2021-10-23 10:07:41 +0100

    Add key

--- src/components/SavedBooks.tsx
+++ src/components/SavedBooks.tsx
@@ -16,10 +16,12 @@ export function SavedBooks() {
 			<div className="grid grid-cols-3 gap-x-4 gap-y-2">
 				{books.map(([id, info]) => (
 					<Link
+						key={id}
 						href={`/read/${id}`}
 						className="bg-gray3 hover:bg-gray4 focus:bg-gray5 text-gray11 transition rounded-lg px-24 py-16 tracking-tight font-medium"
 					>
-						<span className="text-gray12 text-lg">“{info.title ?? "—"}”</span> <br />
+						<span className="text-gray12 text-lg">“{info.title ?? "—"}”</span>{" "}
+						<br />
 						<span className="text-gray12">{info.author ?? "—"}</span>
 					</Link>
 				))}
