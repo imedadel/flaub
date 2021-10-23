3e2ac8861d1ed50e331bfe7074183961d19fc09f
Imed Adel <aadelimed@gmail.com>
2021-10-23 20:13:26 +0100

    Remove unused

--- src/contexts/theme.tsx
+++ src/contexts/theme.tsx
@@ -10,33 +10,22 @@ import React, {
 } from "react"
 
 export interface UseThemeProps {
-	/** List of all available theme names */
-	themes: string[]
 	/** Update the theme */
 	setTheme: (theme: string) => void
 	/** Active theme name */
 	theme?: string
-	/** If `enableSystem` is true and the active theme is "system", this returns whether the system preference resolved to "dark" or "light". Otherwise, identical to `theme` */
-	resolvedTheme?: string
-	/** If enableSystem is true, returns the System theme preference ("dark" or "light"), regardless what the active theme is */
-	systemTheme?: "dark" | "light"
 }
 
 const ThemeContext = createContext<UseThemeProps>({
 	setTheme: (_) => {},
-	themes: [],
 })
 export const useTheme = () => useContext(ThemeContext)
 
 const colorSchemes = ["light", "dark"]
 const MEDIA = "(prefers-color-scheme: dark)"
 
-interface ValueObject {
-	[themeName: string]: string
-}
-
 export const ThemeProvider: React.FC<{}> = ({ children }) => {
-	const disableTransitionOnChange = false
+	const disableTransitionOnChange = true
 	const enableColorScheme = true
 	const storageKey = "theme"
 	const value = {
@@ -158,12 +147,6 @@ export const ThemeProvider: React.FC<{}> = ({ children }) => {
 			value={{
 				theme,
 				setTheme,
-				resolvedTheme: theme === "system" ? resolvedTheme : theme,
-				themes: enableSystem ? [...themes, "system"] : themes,
-				systemTheme: (enableSystem ? resolvedTheme : undefined) as
-					| "light"
-					| "dark"
-					| undefined,
 			}}
 		>
 			{children}
